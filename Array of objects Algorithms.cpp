feb2f2b1b0548d7278e4b8fcd1efe4b279a8c8ec
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2023-08-15 19:49:28 +0530

    Add project files.

---
+++ Array of objects Algorithms.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+class student {
+private:
+    int studentId;
+    string studentName;
+    int stduentmarks;
+
+    student(int StudentId,string StudentName,int Studentmarks ) {
+
+        studentId = StudentId;
+        studentName = StudentName;
+        studentName = Studentmarks;
+
+    }
+};
+int main()
+{
+  
+}
+
+
